1d0fdeb62f9af66c22180806cdcd34ce28bbdea0
Alex <gamer.infinity@hotmail.com>
2025-04-12 14:44:38 -0500

    Cambio1

---
+++ Crypto-IOS/Crypto-IOS/Views/AssetList.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct AssetList: View {
+    
+    var viewModel: AssetListViewModel = .init()
+
+    
+    var body: some View {
+        NavigationStack {
+            
+            Text(viewModel.errorMessage ?? "")
+                
+            List {
+                ForEach(viewModel.assets) { asset in
+                    NavigationLink {
+                        AssetDetailView(asset: asset)
+                    } label: {
+                        AssetView(assetViewState: .init(asset))
+                    }
+                }
+            }
+            .listStyle(.plain)
+            .task {
+               await viewModel.fetchAssets()
+            }
+            .navigationTitle("Home")
+            
+        }
+       
+    }
+}
+
+#Preview {
+    AssetList()
+}
