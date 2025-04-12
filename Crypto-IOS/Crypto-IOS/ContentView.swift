7508e342fe5b8792d6fed9212672d457cfcbcefe
Alex <gamer.infinity@hotmail.com>
2025-04-11 22:52:02 -0500

    Prueba5

---
+++ Crypto-IOS/Crypto-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crypto-IOS
+//
+//  Created by user272166 on 4/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crypto-IOS/Crypto-IOS/Crypto_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_IOSApp.swift
+//  Crypto-IOS
+//
+//  Created by user272166 on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
